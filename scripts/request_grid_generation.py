5fffe2fe46026b917e4228e4a38ea84a428f7c04
Filipe Amaral <filipe.amaral@vwds.pt>
2024-12-16 08:30:35 +0000

    🐛 loop automatic generation when it fails with 500 (server was asleep)

--- scripts/request_grid_generation.py
+++ scripts/request_grid_generation.py
@@ -4,18 +4,25 @@ import requests
 
 load_dotenv()
 
-try:
-    response = requests.post(f'{os.getenv("DOMAIN_URL")}{os.getenv("GRID_GENERATION_ENDPOINT")}', timeout=60)
 
-    if response.status_code == 200:
-        print("Request succeeded with status code:", response.status_code)
-    elif response.status_code >= 400:
-        print("Request failed with status code:", response.status_code)
-    else:
-        print("Received status code:", response.status_code)
+while True:
+    try:
+        response = requests.post(f'{os.getenv("DOMAIN_URL")}{os.getenv("GRID_GENERATION_ENDPOINT")}', timeout=60)
 
-except requests.exceptions.Timeout:
-    print("The request timed out.")
-except requests.exceptions.RequestException as e:
-    # For other errors, print the exception
-    print("An error occurred:", e)
+        if response.status_code == 200:
+            print("Request succeeded with status code:", response.status_code)
+            break
+        elif response.status_code == 500:
+            print("Request failed with status code:", response.status_code)
+        elif response.status_code >= 400:
+            print("Request failed with status code:", response.status_code)
+            break
+        else:
+            print("Received status code:", response.status_code)
+            break
+
+    except requests.exceptions.Timeout:
+        print("The request timed out.")
+    except requests.exceptions.RequestException as e:
+        # For other errors, print the exception
+        print("An error occurred:", e)
