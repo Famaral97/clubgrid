8123c8bca045ebe7d557c1942f166a6d3acfbaab
bandildo <miguel.g.azevedo@gmail.com>
2025-03-04 14:51:57 +0000

    Update ping.py to ping the root page
    In order for it to have a database operation

--- scripts/ping.py
+++ scripts/ping.py
@@ -1,8 +1,8 @@
 import requests
 
-url = "https://clubgrid.pythonanywhere.com/health-check"
+url = "https://clubgrid.pythonanywhere.com/"
 
-response = requests.get(url)
+response = requests.get(url, timeout=5)
 if response.status_code == 200:
     print(f"Success! Status code: {response.status_code}")
 else:
